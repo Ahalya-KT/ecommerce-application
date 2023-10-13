e53dd2745529e2973ad233e007021ef4f24b5b1d
Ahalya-KT <ahalyakt002@gmail.com>
2023-10-13 14:12:54 +0530

    ecommerce

--- src/page/Desgination.js
+++ src/page/Desgination.js
@@ -1,81 +1,86 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../component/Navbar'
-import axios from 'axios'
-import { Edit, Trash2 } from 'react-feather'
+import React, { useEffect, useState } from "react";
+import Navbar from "../component/Navbar";
+import axios from "axios";
+import { Edit, Trash2 } from "react-feather";
 
 function Desgination() {
+  const [name, setname] = useState([]);
 
-    const[name,setname]=useState([])
-
-    useEffect(()=>{
-        axios.get('http://training.pixbit.in/api/designations',{headers:{'Authorization': 'Bearer ' + "asQ1nRbByT8xTRLoqx6cNtiipiramSDHXdQWEhJ2td9DxEz4MEJnBcQaauuKX9qct2GiQKSC91AUWMDb"}})
-        .then(res=>{
+  useEffect(() => {
+    axios
+      .post("http://training.pixbit.in/api/login", {
+        email: "nidhinnp@gmail.com",
+        password: "12345678Np",
+      })
+      .then((user) => {
+        console.log(user.data.data.access_token);
+        let token = user.data.data.access_token;
+        axios
+          .get("http://training.pixbit.in/api/designations", {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          })
+          .then((res) => {
             console.log(res);
-            setname(res.data.data.data)
-        })
-        .catch(err=>{
+            setname(res.data.data.data);
+          })
+          .catch((err) => {
             console.log(err);
-        })
-    })
+          });
+      });
+  }, []);
 
+  // useEffect(()=>{
+  //     axios.get('http://training.pixbit.in/api/designations',{headers:{'Authorization': 'Bearer ' + "asQ1nRbByT8xTRLoqx6cNtiipiramSDHXdQWEhJ2td9DxEz4MEJnBcQaauuKX9qct2GiQKSC91AUWMDb"}})
+  //     .then(res=>{
+  //         console.log(res);
+  //         setname(res.data.data.data)
+  //     })
+  //     .catch(err=>{
+  //         console.log(err);
+  //     })
+  // })
 
   return (
     <div>
-        <Navbar/>
+      <Navbar />
 
+      <div className="desgination-list">
+        <div className="designation-heading">
+          <h2>Employees List</h2>
+          <button>add New Record</button>
+        </div>
 
+        <div className="designation-inputs">
+          <input placeholder="Search"></input>
+        </div>
+      </div>
 
+      <table className="desgination-table">
+        <tr>
+          <th>Sl.No</th>
+          <th>Name</th>
+          <th>Action</th>
+        </tr>
 
-        <div className='desgination-list'>
+        {name &&
+          name.map((post) => (
+            <tr key={post.id}>
+              <td>{post.id}</td>
+              <td>{post.name}</td>
 
-           <div className='designation-heading'>
-            <h2>Employees List</h2>
-            <button>add New Record</button>
-           </div>
-
-           <div className='designation-inputs'>
-           <input placeholder="Search"></input>
-           </div>
-          
-           </div>
-
-       
-
-        <table className='desgination-table'>
-            <tr>
-            <th>Sl.No</th>
-             <th>Name</th>
-             <th>Action</th>
+              <td>
+                <div className="button-btn">
+                  <Edit color="white" />
+                  <Trash2 color="white" />
+                </div>
+              </td>
             </tr>
-   
-   {name &&
-    name.map((post)=>(
-    <tr key={post.id}>
-        <td>{post.id}</td>
-        <td>{post.name}</td>
-
-       
-        <td>
-            <div className='button-btn'>
-
-            <Edit color='white'/>
-        <Trash2 color='white'/>
-            </div>
-
-           
-        </td>
-      
-        
-    </tr>
-   ))}
-</table>
-
-
-
-
-        
+          ))}
+      </table>
     </div>
-  )
+  );
 }
 
-export default Desgination
+export default Desgination;
--- src/page/EmployeList.js
+++ src/page/EmployeList.js
@@ -1,97 +1,86 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../component/Navbar'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import Navbar from "../component/Navbar";
+import axios from "axios";
 
 function EmployeList() {
+  const [employe, setEmploye] = useState([]);
 
-    const[employe,setEmploye]=useState([]) 
-
-    
-
-    useEffect(() => {
+  useEffect(() => {
+    axios
+      .post("http://training.pixbit.in/api/login", {
+        email: "nidhinnp@gmail.com",
+        password: "12345678Np",
+      })
+      .then((user) => {
+        console.log(user.data.data.access_token);
+        let token = user.data.data.access_token;
         axios
-          .post("http://training.pixbit.in/api/login", {
-            email: "nidhinnp@gmail.com",
-            password: "12345678Np",
+          .get("http://training.pixbit.in/api/employees", {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
           })
-          .then((user) => {
-            // console.log(user.data.data.access_token);
-            let token = user.data.data.access_token;
-            axios
-              .get("http://training.pixbit.in/api/designations", {
-                headers: {
-                  Authorization: Bearer ${token},
-                },
-              })
-              .then((res) => {
-                console.log(res);
-                setname(res.data.data.data);
-              })
-              .catch((err) => {
-                console.log(err);
-              });
-          });
-      }, []);
-    // useEffect(()=>{
-    //     axios.get('http://training.pixbit.in/api/employees',{headers:{"Authorization": 'Bearer ' + "asQ1nRbByT8xTRLoqx6cNtiipiramSDHXdQWEhJ2td9DxEz4MEJnBcQaauuKX9qct2GiQKSC91AUWMDb"}})
-    //     .then(res=>{
-    //         console.log(res.data.data);
-    //         setEmploye(res.data.data.data)
-    //     })
-    //     .catch(err =>{
-    //         console.log(err);
-    //     })
-    // })
+          .then((res) => {
+            console.log(res);
+            setEmploye(res.data.data.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+  }, []);
 
-    
-  
+  // useEffect(()=>{
+  //     axios.get('http://training.pixbit.in/api/employees',{headers:{"Authorization": 'Bearer ' + "asQ1nRbByT8xTRLoqx6cNtiipiramSDHXdQWEhJ2td9DxEz4MEJnBcQaauuKX9qct2GiQKSC91AUWMDb"}})
+  //     .then(res=>{
+  //         console.log(res.data.data);
+  //         setEmploye(res.data.data.data)
+  //     })
+  //     .catch(err =>{
+  //         console.log(err);
+  //     })
+  // })
+
   return (
     <div>
-        <Navbar/>
+      <Navbar />
 
-        <div className='employee-list'>
+      <div className="employee-list">
+        <div className="employe-heading">
+          <h2>Employees List</h2>
+          <button>add New Record</button>
+        </div>
 
-           <div className='employe-heading'>
-            <h2>Employees List</h2>
-            <button>add New Record</button>
-           </div>
+        <div className="employee-inputs">
+          <input placeholder="Search"></input>
+        </div>
+      </div>
 
-           <div className='employee-inputs'>
-           <input placeholder="Search"></input>
-           </div>
-          
-           </div>
-
-
-            <table className='employe-table'>
-                <tr>
-                    <th>Sl.No</th>
-                    <th> First Name</th>
-                    <th> Last Name</th>
-                    <th>Join Date</th>
-                    <th>Date of Birth</th>
-                    <th>Gender</th>
-                    <th>Designation</th>
-                </tr>
-                {employe && employe.map((post)=>(
-                    <tr key={post.id}>
-                        <td>{post.id}</td>
-                        <td>{post.first_name}</td>
-                        <td>{post.last_name}</td>
-                        <td>{post.join_date}</td>
-                        <td>{post.date_of_birth}</td>
-                        <td>{post.gender}</td>
-                        <td>{post.designation_id}</td>
-                       
-                    </tr>
-                ))}
-            </table>
-
-
-
-       
+      <table className="employe-table">
+        <tr>
+          <th>Sl.No</th>
+          <th> First Name</th>
+          <th> Last Name</th>
+          <th>Join Date</th>
+          <th>Date of Birth</th>
+          <th>Gender</th>
+          <th>Designation</th>
+        </tr>
+        {employe &&
+          employe.map((post) => (
+            <tr key={post.id}>
+              <td>{post.id}</td>
+              <td>{post.first_name}</td>
+              <td>{post.last_name}</td>
+              <td>{post.join_date}</td>
+              <td>{post.date_of_birth}</td>
+              <td>{post.gender}</td>
+              <td>{post.designation_id}</td>
+            </tr>
+          ))}
+      </table>
     </div>
-  )
+  );
 }
 
-export default EmployeList
+export default EmployeList;
